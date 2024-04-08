3c23d0bf1e7667f0e6ba717b963391ff98107cf5
Soko77788 <NickSokolowski7@gmail.com>
2024-04-07 21:13:21 -0500

    api call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,39 @@
 
-https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={40591cbd3b73cc2ec73be5cdc2703ba9}
 
 
 
-console.log('turkey')
+const searchInput = document.getElementById('citySearch')
+const searchBtn = document.getElementById('searchBtn')
+const APIkey = '40591cbd3b73cc2ec73be5cdc2703ba9'
+const city = searchInput.value
+
+
+searchBtn.addEventListener('click', function(){
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&appid=${APIkey}&units=imperial`)
+                .then(function(response){
+                    return response.json()
+                })
+                .then(function(results){
+                    console.log(results)
+            
+                })
+
+})
+
+
+// function handleSubmit(event) {
+//     event.preventDefault()
+//     const citySearch = searchInput.value
+//     console.log(citySearch)
+
+//     if (citySearch) {
+//         location.href = `./index.html?citySearch=${citySearch}`
+//         }
+
+//     }
+
+
+// console.log(searchBtn, searchInput)
+
+
+// console.log('turkey')
