a43f4ba06114aa5d0d9312b8a108ce2d04f08639
Soko77788 <NickSokolowski7@gmail.com>
2024-04-11 07:30:27 -0500

    dates on small cards adjusted

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,25 +22,24 @@ searchBtn.addEventListener("click", function () {
     .then(function (data) {
       console.log(data);
       //loop for dates in the cards
-      for (let i = 0; i < data.list.length; i+=8) {
-        document.getElementById("card-date" + ((i / 8) + 1)).innerHTML =
-          "Date: " + data.list[i].dt_txt;
-          //loop for weather icon on cards
-          document.getElementById("card-weatherIcon" + ((i / 8) + 1)).src =
-            "http://openweathermap.org/img/wn/" +
-            data.list[i].weather[0].icon +
-            ".png";
-            //loop for temp on cards
-            document.getElementById("card-temp" + ((i / 8) + 1)).innerHTML =
-              "Temp: " + data.list[i].main.temp + "°";
-              //loop for wind speed on card
-              document.getElementById("card-wind" + ((i / 8) + 1)).innerHTML =
-                "Wind: " + data.list[i].wind.speed + "MPH";
-                //loop for humidity on card
-                document.getElementById("card-humidity" + ((i / 8) + 1)).innerHTML =
-                  "Humidity: " + data.list[i].main.humidity;
+      for (let i = 0; i < data.list.length; i += 8) {
+        document.getElementById("card-date" + (i / 8 + 1)).innerHTML =
+          data.list[i].dt_txt.slice(0, 10);
+        //loop for weather icon on cards
+        document.getElementById("card-weatherIcon" + (i / 8 + 1)).src =
+          "http://openweathermap.org/img/wn/" +
+          data.list[i].weather[0].icon +
+          ".png";
+        //loop for temp on cards
+        document.getElementById("card-temp" + (i / 8 + 1)).innerHTML =
+          "Temp: " + data.list[i].main.temp + "°";
+        //loop for wind speed on card
+        document.getElementById("card-wind" + (i / 8 + 1)).innerHTML =
+          "Wind: " + data.list[i].wind.speed + "MPH";
+        //loop for humidity on card
+        document.getElementById("card-humidity" + (i / 8 + 1)).innerHTML =
+          "Humidity: " + data.list[i].main.humidity;
       }
-  
 
       // store lat and lon for 2nd fetch
       const lat = data.city.coord.lat;
@@ -56,18 +55,18 @@ searchBtn.addEventListener("click", function () {
           // Displaying city followed by the date
           const currentDate = new Date(currentData.dt * 1000); // Convert timestamp to milliseconds
 
-          
-document.getElementById(
-            "cityDate"
-          ).innerHTML = `${currentData.name} (${currentDate.toDateString()})`;
+          document.getElementById("cityDate").innerHTML = `${
+            currentData.name
+          } (${currentDate.toDateString()})`;
           document.getElementById(
             "cityDate"
           ).innerHTML += `<img id="weatherIconMain" src="http://openweathermap.org/img/wn/${currentData.weather[0].icon}.png">`;
-        
-        mainTemp.innerHTML = "Temp: " + currentData.main.temp + '°'
-        mainWind.innerHTML = "Wind: " + currentData.wind.speed + ' MPH'
-        mainHumidity.innerHTML = "Humidity: " + currentData.main.humidity + "%"
-          
+
+          mainTemp.innerHTML = "Temp: " + currentData.main.temp + "°";
+          mainWind.innerHTML = "Wind: " + currentData.wind.speed + " MPH";
+          mainHumidity.innerHTML =
+            "Humidity: " + currentData.main.humidity + "%";
+
           console.log(currentData);
         })
         .catch(function (error) {
@@ -79,123 +78,109 @@ document.getElementById(
     });
 });
 
-// function handleSubmit(event) {
-//     event.preventDefault()
-//     const citySearch = searchInput.value
-//     console.log(citySearch)
-
-//     if (citySearch) {
-//         location.href = `./index.html?citySearch=${citySearch}`
-//         }
-
-//     }
-
-
-const searchedCitiesDiv = document.getElementById('searchedCity');
+const searchedCitiesDiv = document.getElementById("searchedCity");
 
 // Listen for clicks on the search button
-searchBtn.addEventListener('click', function () {
-    const searchedCity = searchInput.value.trim();
-    if (searchedCity) {
-        // Store the searched city in local storage
-        let searchedCities = JSON.parse(localStorage.getItem('searchedCities')) || [];
-        searchedCities.push(searchedCity);
-        localStorage.setItem('searchedCities', JSON.stringify(searchedCities));
+searchBtn.addEventListener("click", function () {
+  const searchedCity = searchInput.value.trim();
+  if (searchedCity) {
+    // Store the searched city in local storage
+    let searchedCities =
+      JSON.parse(localStorage.getItem("searchedCities")) || [];
+    searchedCities.push(searchedCity);
+    localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
 
-        // Render the searched cities from local storage
-        renderSearchedCities();
+    // Render the searched cities from local storage
+    renderSearchedCities();
 
-        // Clear the search input
-        searchInput.value = '';
-    }
+    // Clear the search input
+    searchInput.value = "";
+  }
 });
 
 // Render the searched cities from local storage
 function renderSearchedCities() {
-    searchedCitiesDiv.innerHTML = '';
-    const searchedCities = JSON.parse(localStorage.getItem('searchedCities')) || [];
-    searchedCities.forEach(city => {
-        
-        const cityButton = document.createElement('button');
-        cityButton.textContent = city;
-        cityButton.classList.add('btn', 'btn-secondary', 'm-1');
-        cityButton.addEventListener('click', function () {
-            // Retrieve weather data for the clicked city and display it
-            fetchWeatherData(city);
-        });
-        searchedCitiesDiv.appendChild(cityButton);
+  searchedCitiesDiv.innerHTML = "";
+  const searchedCities =
+    JSON.parse(localStorage.getItem("searchedCities")) || [];
+  searchedCities.forEach((city) => {
+    const cityButton = document.createElement("button");
+    cityButton.textContent = city;
+    cityButton.classList.add("btn", "btn-secondary", "m-1");
+    cityButton.addEventListener("click", function () {
+      // Retrieve weather data for the clicked city and display it
+      fetchWeatherData(city);
     });
+    searchedCitiesDiv.appendChild(cityButton);
+  });
 }
 
 // Function to fetch weather data for a city
 function fetchWeatherData(city) {
-    fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}&units=imperial&`
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}&units=imperial&`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      updateUi(data);
+      console.log(data);
+
+      const lat = data.city.coord.lat;
+      const lon = data.city.coord.lon;
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`
       )
         .then(function (response) {
           return response.json();
         })
-        .then(function (data) {
-            updateUi(data)
-          console.log(data);
+        .then(function (currentData) {
+          updateUi2(currentData);
+          console.log(currentData);
+        });
 
-          const lat = data.city.coord.lat;
-      const lon = data.city.coord.lon;
-
-    
-
-          fetch(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`
-          )
-            .then(function (response) {
-              return response.json();
-            })
-            .then(function (currentData) {
-                updateUi2(currentData)
-                console.log(currentData)
-            })
-        
-    // Fetch weather data for the specified city
-    // Display the weather data in the appropriate sections
-})
+      // Fetch weather data for the specified city
+      // Display the weather data in the appropriate sections
+    });
 }
 
 function updateUi(data) {
-    for (let i = 0; i < data.list.length; i+=8) {
-        document.getElementById("card-date" + ((i / 8) + 1)).innerHTML =
-          "Date: " + data.list[i].dt_txt;
-          //loop for weather icon on cards
-          document.getElementById("card-weatherIcon" + ((i / 8) + 1)).src =
-            "http://openweathermap.org/img/wn/" +
-            data.list[i].weather[0].icon +
-            ".png";
-            //loop for temp on cards
-            document.getElementById("card-temp" + ((i / 8) + 1)).innerHTML =
-              "Temp: " + data.list[i].main.temp + "°";
-              //loop for wind speed on card
-              document.getElementById("card-wind" + ((i / 8) + 1)).innerHTML =
-                "Wind: " + data.list[i].wind.speed + "MPH";
-                //loop for humidity on card
-                document.getElementById("card-humidity" + ((i / 8) + 1)).innerHTML =
-                  "Humidity: " + data.list[i].main.humidity;
-      }
-    
+  for (let i = 0; i < data.list.length; i += 8) {
+    document.getElementById("card-date" + (i / 8 + 1)).innerHTML = data.list[
+      i
+    ].dt_txt.slice(0, 10);
+    //loop for weather icon on cards
+    document.getElementById("card-weatherIcon" + (i / 8 + 1)).src =
+      "http://openweathermap.org/img/wn/" +
+      data.list[i].weather[0].icon +
+      ".png";
+    //loop for temp on cards
+    document.getElementById("card-temp" + (i / 8 + 1)).innerHTML =
+      "Temp: " + data.list[i].main.temp + "°";
+    //loop for wind speed on card
+    document.getElementById("card-wind" + (i / 8 + 1)).innerHTML =
+      "Wind: " + data.list[i].wind.speed + "MPH";
+    //loop for humidity on card
+    document.getElementById("card-humidity" + (i / 8 + 1)).innerHTML =
+      "Humidity: " + data.list[i].main.humidity;
+  }
 }
 
 function updateUi2(currentData) {
-    const currentDate = new Date(currentData.dt * 1000); // Convert timestamp to milliseconds
+  const currentDate = new Date(currentData.dt * 1000); // Convert timestamp to milliseconds
 
-          
-document.getElementById(
-            "cityDate"
-          ).innerHTML = `${currentData.name} (${currentDate.toDateString()})`;
-          document.getElementById(
-            "cityDate"
-          ).innerHTML += `<img id="weatherIconMain" src="http://openweathermap.org/img/wn/${currentData.weather[0].icon}.png">`;
-        
-        mainTemp.innerHTML = "Temp: " + currentData.main.temp + '°'
-        mainWind.innerHTML = "Wind: " + currentData.wind.speed + ' MPH'
-        mainHumidity.innerHTML = "Humidity: " + currentData.main.humidity + "%"
+  document.getElementById("cityDate").innerHTML = `${
+    currentData.name
+  } (${currentDate.toDateString()})`;
+  document.getElementById(
+    "cityDate"
+  ).innerHTML += `<img id="weatherIconMain" src="http://openweathermap.org/img/wn/${currentData.weather[0].icon}.png">`;
+
+  mainTemp.innerHTML = "Temp: " + currentData.main.temp + "°";
+  mainWind.innerHTML = "Wind: " + currentData.wind.speed + " MPH";
+  mainHumidity.innerHTML = "Humidity: " + currentData.main.humidity + "%";
 }
 
 // Initial rendering of searched cities from local storage
